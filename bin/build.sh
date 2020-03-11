9f1cab5190b0609694ad2553b21395e6212081d8
Vincent <viha195@gmail.com>
2020-03-11 09:52:29 +0700

    skip install sdk

--- bin/build.sh
+++ bin/build.sh
@@ -70,21 +70,21 @@ MAGENTO_URL="http://$NODE_IP:$PORT"
 
 if [[ ! -z "${GITHUB_BRANCH}" ]]; then
     # Install required module
-    docker-compose exec -u www-data -T magento bash -c \
-        "echo '{ \
-            \"http-basic\": { \
-                \"repo.magento.com\": { \
-                    \"username\": \"a3380186b4ffb670466a01331a3fb375\", \
-                    \"password\": \"cfe4874a50552827da901971d249322a\" \
-                } \
-            } \
-        }' > auth.json ; \
-        php vendor/composer/composer/bin/composer require \
-        authorizenet/authorizenet \
-        paypal/rest-api-sdk-php:* \
-        paypal/merchant-sdk-php:* \
-        stripe/stripe-php:* \
-        zendframework/zend-barcode;"
+#    docker-compose exec -u www-data -T magento bash -c \
+#        "echo '{ \
+#            \"http-basic\": { \
+#                \"repo.magento.com\": { \
+#                    \"username\": \"a3380186b4ffb670466a01331a3fb375\", \
+#                    \"password\": \"cfe4874a50552827da901971d249322a\" \
+#                } \
+#            } \
+#        }' > auth.json ; \
+#        php vendor/composer/composer/bin/composer require \
+#        authorizenet/authorizenet \
+#        paypal/rest-api-sdk-php:* \
+#        paypal/merchant-sdk-php:* \
+#        stripe/stripe-php:* \
+#        zendframework/zend-barcode;"
 fi
 
 if [ $? -ne 0 ]; then
